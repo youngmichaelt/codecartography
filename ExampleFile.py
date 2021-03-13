8e324ec7fd7554db87a0e131562bd2d78513d47d
Mac <mac@Macs-MacBook.local>
2021-03-13 09:52:43 -0500

    Added ExampleFile.py to be read and mapped with CodeCartography for testing.

---
+++ ExampleFile.py
@@ -0,0 +1,15 @@
+##This is an example file to be read and mapped by CodeCartography
+
+stringExample = "Hello World"
+
+intExample = 10
+
+def functionExample(stringExample, intExample):
+
+    print(stringExample)
+    print(intExample)
+
+    return stringExample, intExample
+
+
+functionExample(stringExample, intExample)
