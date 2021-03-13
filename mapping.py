68c216a211c702ef904a2fb23e4a3975eaaba5bd
Mac <mac@Macs-MacBook.local>
2021-03-13 10:59:49 -0500

    Adding functionality to create a dataframe from ExampleFile.py for mapping

--- mapping.py
+++ mapping.py
@@ -1,2 +1,77 @@
 import os
+import pandas as pd
+import numpy as np
+
+path = "/Users/mac/Desktop/repos/codecartography/ExampleFile.py"
+
+##Open .py file and read line by line
+with open (path) as f:
+    content = f.readlines()
+
+print(content)
+
+lines = []
+
+for line in content:
+    x = line.replace('  ', 'TAB')
+    lines.append(x)
+
+
+parents = []
+children = []
+items = []
+values = []
+
+for line in lines:
+
+    counter = 0
+    temp = []
+
+    if ("#" in line):
+        continue
+
+    if ('TAB' not in line):
+        parents.append("NA")
+        items.append(line)
+        values.append(line)
+
+        for x in lines:
+
+            if lines.index(x) <= lines.index(line): continue
+
+            print(x)
+            if ('TAB' in x):
+                temp.append(x)
+
+            else:
+                continue
+
+        children.append(temp)
+
+
+    
+
+    
+
+
+
+
+df = pd.DataFrame(columns=['Item', 'Parent', 'Children', 'Value'])
+
+data = pd.DataFrame({'Item': items, 'Parent': parents, 'Children': children, 'Value': values})
+
+df = df.append(data)
+
+pd.set_option("display.max_rows", None, "display.max_columns", None)
+
+
+print(df)
+
+df.to_csv("/Users/mac/Desktop/repos/codecartography/output.csv")
+
+
+##how to structure this dataframe
+
+
+#Item Parent Children Value
 
